5261c398f2c81334ec0ac293bb30f3cd8f34daee
MountainBridge <nidhi199.verma@gmail.com>
2022-12-24 23:51:53 +0530

    removing express validator

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,14 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { check, validationResult } = require('express-validator');
+const { check, validationResult } = require('express-validator/check');
 
 //@post api/users, @access Public, @desc test route
-router.post('/', 
-[
-    check(name, 'Name is required').not().isEmpty(),
-    check(email, 'Please include a valid email').isEmail(),
-    check(password, 'Please enter a password with 6 or more characters').isLength({min:6})
-],
+router.post('/',
 (req, res) => {
     console.log("request body", req.body);
     res.send("get users")});
