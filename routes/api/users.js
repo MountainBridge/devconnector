a1c021d9f58c75cd71679f7b077eb0da0ffd97ef
MountainBridge <nidhi199.verma@gmail.com>
2022-12-25 12:59:23 +0530

    express validator to check users input request

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,8 +3,17 @@ const router = express.Router();
 const { check, validationResult } = require('express-validator/check');
 
 //@post api/users, @access Public, @desc test route
+//adding validations on the request received for users
 router.post('/',
+[   check('name', 'Name is required').not().isEmpty(),
+    check('email', 'Please enter a valid email').isEmail(),
+    check('password', 'Please enter a password with 6 or more characters').isLength({min:6})
+],
 (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array()});
+    }
     console.log("request body", req.body);
     res.send("get users")});
 
