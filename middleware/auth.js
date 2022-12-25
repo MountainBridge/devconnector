b61a2ce08269a91a1fb0f4a43ae297579ac860fb
MountainBridge <nidhi199.verma@gmail.com>
2022-12-25 22:55:59 +0530

    adding middleware to authenticate the token

---
+++ middleware/auth.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
+//middleware has access to req and res objects and next is the callback to the next piece of middleware
+module.exports = function(req, res, next) {
+    //get the token from the header
+    const token = req.header('x-auth-token');
+
+    //check if token exists
+    if(!token){
+        res.status(401).json({msg: 'No token, authorization denied'});
+    }
+    //verify token
+    try{
+        const decoded = jwt.verify(token, config.get('jwtSecret'));
+        req.user = decoded.user;
+        console.log('user:', req.user);
+        next();
+    }catch(err){
+        res.status(401).json({msg: 'token is invalid'});
+    }
+}
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
 
 //@get api/auth, @access Public, @desc test route
-router.get('/', (req, res) => res.send("get auth"));
+router.get('/', auth, (req, res) => res.send("get auth"));
 
 module.exports = router;
