677cc9a250cc8a416a1dd0e0db6031906aedd21f
MountainBridge <nidhi199.verma@gmail.com>
2022-12-25 14:53:21 +0530

    registering user, encrypting pwd, check for duplicate users

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const { check, validationResult } = require('express-validator/check');
+const User = require('../../models/User');
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
 
 //@post api/users, @access Public, @desc test route
 //adding validations on the request received for users
@@ -9,12 +12,42 @@ router.post('/',
     check('email', 'Please enter a valid email').isEmail(),
     check('password', 'Please enter a password with 6 or more characters').isLength({min:6})
 ],
-(req, res) => {
+async (req, res) => {
     const errors = validationResult(req);
     if(!errors.isEmpty()){
         return res.status(400).json({errors: errors.array()});
     }
-    console.log("request body", req.body);
-    res.send("get users")});
 
+    const {name, email, password} = req.body;
+
+    try{
+    //see if user exists
+    let user = await User.findOne({email});
+    if(user){
+        return res.status(400).json({errors: [{msg:"User already exists"}]});
+    }
+
+    //get users gravatar
+    const avatar = gravatar.url(email, {
+        s: '200',
+        r: 'pg',
+        d: 'mm'
+    })
+
+    user = new User({name, email, avatar, password});
+
+    //** anything returning a promise must be appended with an await **/
+    //encrypt password
+    let salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(password, salt);
+    await user.save();
+
+    //return jsonwebtoken
+
+    res.send("User is registered");
+    } catch(err){
+        console.log(err.message);
+        res.status(500).send('Server error');
+    }
+});
 module.exports = router;
