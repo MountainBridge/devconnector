ef4b3b7e21c96e4e70cbbbb6a07ccf51e3d6b1b5
MountainBridge <nidhi199.verma@gmail.com>
2022-12-25 23:51:43 +0530

    get authenticated user

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
+const User = require('../../models/User');
 
 //@get api/auth, @access Public, @desc test route
-router.get('/', auth, (req, res) => res.send("get auth"));
-
+router.get('/', auth, async (req, res) =>{
+try{
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+}catch(err){
+    console.error(err.message);
+    res.status(500).send('Server error');
+}
+});
 module.exports = router;
