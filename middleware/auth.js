2e7f0dfc904d1360fdf5a9dc55d8314e57784ce8
MountainBridge <nidhi199.verma@gmail.com>
2022-12-30 00:51:49 +0530

    adding auth to profile & adding /me endpoint to fetch profile info

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,21 +1,27 @@
 const jwt = require('jsonwebtoken');
 const config = require('config');
 
-//middleware has access to req and res objects and next is the callback to the next piece of middleware
-module.exports = function(req, res, next) {
-    //get the token from the header
-    const token = req.header('x-auth-token');
+module.exports = function (req, res, next) {
+  // Get token from header
+  const token = req.header('x-auth-token');
 
-    //check if token exists
-    if(!token){
-        res.status(401).json({msg: 'No token, authorization denied'});
-    }
-    //verify token
-    try{
-        const decoded = jwt.verify(token, config.get('jwtSecret'));
+  // Check if not token
+  if (!token) {
+    return res.status(401).json({ msg: 'No token, authorization denied' });
+  }
+
+  // Verify token
+  try {
+    jwt.verify(token, config.get('jwtSecret'), (error, decoded) => {
+      if (error) {
+        return res.status(401).json({ msg: 'Token is not valid' });
+      } else {
         req.user = decoded.user;
-        console.log('user:', req.user);
-    }catch(err){
-        res.send.status(401).json({msg: 'token is invalid'});
-    }
-}
+        next();
+      }
+    });
+  } catch (err) {
+    console.error('something wrong with auth middleware');
+    res.status(500).json({ msg: 'Server Error' });
+  }
+};
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,7 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const auth = require("../../middleware/auth");
+const Profile = require("../../models/Profile");
+const User = require("../../models/User");
 
-//@get api/profile, @access Public, @desc test route
-router.get('/', (req, res) => res.send("get profiles"));
+//@get api/profile/me,
+//@access Private, 
+//@desc get current users profile
+router.get('/me', auth, async (req, res) => {
+    try {
+      const profile = await Profile.findOne({
+        user: req.user.id
+      }).populate('user', ['name', 'avatar']);
+  
+      if (!profile) {
+        return res.status(400).json({ msg: 'There is no profile for this user' });
+      }
+  
+      res.json(profile);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  });
 
 module.exports = router;
