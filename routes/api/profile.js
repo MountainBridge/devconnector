ac9bb3a2d8776869454a9c821d23939053d4a853
MountainBridge <nidhi199.verma@gmail.com>
2023-01-02 09:55:10 +0530

    create profile api request

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { check, validationResult } = require('express-validator/check');
 const router = express.Router();
 const auth = require("../../middleware/auth");
 const Profile = require("../../models/Profile");
@@ -24,4 +25,24 @@ router.get('/me', auth, async (req, res) => {
     }
   });
 
+//@post api/profile,
+//@access Private, 
+//@desc create and update users profile
+router.post('/', [
+    auth, 
+    [
+        check('status', 'Status is required')
+        .not()
+        .isEmpty(),
+        check('skills', 'Skills is required')
+        .not()
+        .isEmpty()
+    ]
+], 
+async(req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array()})
+    }
+})
 module.exports = router;
